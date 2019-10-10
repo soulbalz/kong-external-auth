e95d1ccb9a8af294af9953a38d2acf10c9019903
Peerachat Ongya <soulister.o@gmail.com>
2019-10-10 12:35:10 +0700

    Update handler.lua

--- plugins/kong/plugins/external-auth/handler.lua
+++ plugins/kong/plugins/external-auth/handler.lua
@@ -9,11 +9,13 @@ ExternalAuthHandler.VERSION = "1.0.0"
 ExternalAuthHandler.PRIORITY = 900
 
 local function is_ignore_path(request_path, paths)
-  for i, path in ipairs(paths) do
-    local path_after_replace = path:gsub('*', '.*')
-    local match_path = string.match(request_path, path_after_replace)
-    if request_path == match_path then
-      return true
+  if paths then
+    for i, path in ipairs(paths) do
+      local path_after_replace = path:gsub('*', '.*')
+      local match_path = string.match(request_path, path_after_replace)
+      if request_path == match_path then
+        return true
+      end
     end
   end
   return false
@@ -29,8 +31,12 @@ function ExternalAuthHandler:access(conf)
   local request_path = kong.request.get_path()
 
   if is_ignore_path(request_path, conf.no_verify_paths) == false then
-    local request_headers = kong.request.get_headers()
-    request_headers['X-Forwarded-Ssl'] = 'on'
+    local request_auth_header = kong.request.get_header('Authorization')
+
+    local request_headers = {
+      content_type = "application/json",
+      authorization = request_auth_header,
+    }
 
     local client = http.new()
     client:set_timeouts(conf.connect_timeout, conf.send_timeout, conf.read_timeout)
@@ -39,10 +45,7 @@ function ExternalAuthHandler:access(conf)
       method = "POST",
       path = conf.auth_service_path,
       headers = request_headers,
-      ssl_verify = false,
     })
-
-    print(res.body)
 
     if not res then
       return kong.response.exit(500, {message=err})
